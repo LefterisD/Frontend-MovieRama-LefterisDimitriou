7df67f47e81549b8473f8a6d2e0f22d3e54431cc
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-01-19 17:23:40 +0200

    replace # with _

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,13 +1,13 @@
 class SearchView {
-  #parentElement = document.querySelector('.form__container__form');
+  _parentElement = document.querySelector('.form__container__form');
 
   getQuery() {
-    return this.#parentElement.querySelector('.form__container__form__input')
+    return this._parentElement.querySelector('.form__container__form__input')
       .value.trim();
   }
 
   addHandlerSearch(handler) {
-    this.#parentElement
+    this._parentElement
       .querySelector('.form__container__form__input')
       .addEventListener('input', handler);
   }
